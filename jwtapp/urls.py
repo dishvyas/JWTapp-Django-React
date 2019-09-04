7c3c6b87d0f26ed8635525a7a15d75f6733bdcf7
Dishant <dishant.vyas93@gmail.com>
2019-09-05 02:03:23 +0530

    First commit

---
+++ jwtapp/urls.py
@@ -0,0 +1,11 @@
+"""jwtapp URL Configuration
+"""
+from django.contrib import admin
+from django.urls import path
+from rest_framework_jwt.views import obtain_jwt_token
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('token-auth/', obtain_jwt_token),
+    path('users/', include('users.urls'))
+]
